13f44761a1c63ebb853b83f3962ec69a36515aba
Anh Tu <ngqanhtu@outlook.com>
2017-05-26 14:46:31 +0700

    fix config

--- app/models/db.js
+++ app/models/db.js
@@ -4,7 +4,7 @@ var pg = require('pg');
 // and client options
 // note: all config is optional and the environment variables
 // will be read if the config is not present
-var config = require('../config/database');
+var config = require('../../config/database');
 
 if(process.env.DATABASE_URL){
     config = {
@@ -13,8 +13,8 @@ if(process.env.DATABASE_URL){
     password: '518b93ef7c9c3956d65b34ca0579b7974823ccc276358f2d8c2fbd6e5f275f9d', //env var: PGPASSWORD
     host: 'ec2-107-21-108-204.compute-1.amazonaws.com', // Server hosting the postgres database
     port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+   // max: 10, // max number of clients in the pool
+    //idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 }
 var pool = new pg.Pool(config);
