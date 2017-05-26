8ce6126758459fde1ef6c8093449ffe6f1fc6ede
Anh Tu <ngqanhtu@outlook.com>
2017-05-26 21:16:46 +0700

    add form send msg

--- app/controllers/newMessageController.js
+++ app/controllers/newMessageController.js
@@ -1,8 +1,11 @@
-//var Message = require('../models/message');
+var Message = require('../models/message');
+var User = require('../models/user');
 
 var newMessagesController = {
-	index: function (req, res) {
-			res.render('newMessage');
-	}
+    index: function (req, res) {
+        User.getFriends(req.user.id, function (err, friends) {
+            res.render('newMessage', { friends: friends });
+        });
+    },
 }
 module.exports = newMessagesController;
